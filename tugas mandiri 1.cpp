9598ef65915d247a22d15ec7a211a21034384118
unknown <ikhsanulif07@gmail.com>
2024-09-25 20:49:24 +0700

    first commit

---
+++ tugas mandiri 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	char Nama[]= "Ikhsanul Hakim";
+	char Jurusan[]= "Teknik Informatika";
+	char Nim[]= "2441091";
+	
+	printf("Nama	:%s\n", Nama);
+	printf("Jurusan	:%s\n", Jurusan);
+	printf("Nim	:%s\n", Nim);
+	
+	return 0;
+	
+}
